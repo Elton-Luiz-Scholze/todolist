dcaf336277e66820d6c3e200413797415f03d3e1
Elton Luiz Scholze <elscholze@gmail.com>
2023-10-12 19:21:14 -0300

    feat: created Tasks model, controller and Filter

--- src/main/java/br/com/eltonscholze/todolist/user/UserController.java
+++ src/main/java/br/com/eltonscholze/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -19,9 +21,14 @@ public class UserController {
     public ResponseEntity create (@RequestBody UserModel userModel) {
         var findUser = this.userRepository.findByUsername(userModel.getUsername());
 
+        var hashPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(hashPassword);
+
         if(findUser != null) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Usuário já existe");
         }
+        
         var userCreated = this.userRepository.save(userModel);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
